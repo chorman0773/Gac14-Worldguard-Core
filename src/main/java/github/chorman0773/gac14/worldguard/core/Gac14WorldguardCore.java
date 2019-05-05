73a1108608716461c66dc3dc0f127ddc2d467ebd
DragonDreamer <DragonDreamer@DESKTOP-35RCK5J>
2019-05-04 22:44:36 -0400

    Updated WorldGuardCore, WorldGuardModule.

--- src/main/java/github/chorman0773/gac14/worldguard/core/Gac14WorldguardCore.java
+++ src/main/java/github/chorman0773/gac14/worldguard/core/Gac14WorldguardCore.java
@@ -1,9 +1,16 @@
 package github.chorman0773.gac14.worldguard.core;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import github.chorman0773.gac14.Gac14Module;
+
+
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("gac14-worldguard-core")
@@ -24,6 +31,25 @@ public class Gac14WorldguardCore
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
-    	
+    	@SubscribeEvent
+    	public static void registerModule(RegistryEvent.Register<Gac14Module<?>> register) {
+    	   register.getRegistry().register(Gac14WorldguardCore.getInstance().getModule());
+       }
     }
+    
+    private static Gac14WorldguardCore instance;
+    
+    public static Gac14WorldguardCore getInstance(){
+    	assert instance != null;
+    	return instance;	
+    }
+    
+    private WorldGuardModule World = new WorldGuardModule();
+    
+    public WorldGuardModule getModule() {
+    	return World;
+    }
+    
+    
+    
 }
---
+++ src/main/java/github/chorman0773/gac14/worldguard/core/WorldGuardModule.java
@@ -0,0 +1,25 @@
+package github.chorman0773.gac14.worldguard.core;
+
+import github.chorman0773.gac14.Gac14Module;
+import github.chorman0773.gac14.Version;
+import net.minecraft.util.ResourceLocation;
+
+public final class WorldGuardModule extends Gac14Module<WorldGuardModule> {
+
+	public WorldGuardModule() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ResourceLocation getModuleName() {
+		// TODO Auto-generated method stub
+		return ResourceLocation.makeResourceLocation("gac14:worldguard/core");
+	}
+
+	@Override
+	public Version getModuleVersion() {
+		// TODO Auto-generated method stub
+		return new Version("1.0");
+	}
+
+}
