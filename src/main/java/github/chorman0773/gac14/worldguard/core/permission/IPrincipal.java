f39523b8238b49392aaa1d00e2f5d3e42c4a1d13
Connor Horman <chorman64@gmail.com>
2019-05-23 09:03:58 -0400

    Add a check list to PermissionEntry and start documentation

--- src/main/java/github/chorman0773/gac14/worldguard/core/permission/IPrincipal.java
+++ src/main/java/github/chorman0773/gac14/worldguard/core/permission/IPrincipal.java
@@ -6,8 +6,30 @@ import java.util.UUID;
 
 import net.minecraft.util.ResourceLocation;
 
+/**
+ * 
+ * Represents a principal, or something which can be given permissions in Worldguard.
+ * 
+ * A Principal can be a single user, a group, or or even something special such as a Faction.
+ * Instances of this class are specific principals, that is, they have data associated with them.
+ * For Providers see {@link IPrincipalProvider}
+ * 
+ * @author chorm
+ *
+ * @see IPrincipalProvider for the interface that provides Principals.
+ */
 public interface IPrincipal {
+	/**
+	 * Gets the name of the Principal.
+	 * The name of a principal is implementation-defined
+	 */
 	public String getName();
+	
+	/**
+	 * Gets the provider of this Principal.
+	 * 
+	 * The Provider defines how to obtain instances of a Principal, and what data the principal stores.
+	 */
 	public IPrincipalProvider getProvider();
 	public boolean matches(UUID player);
 	public default void store(DataOutputStream strm) throws IOException {
--- src/main/java/github/chorman0773/gac14/worldguard/core/permission/PermissionEntry.java
+++ src/main/java/github/chorman0773/gac14/worldguard/core/permission/PermissionEntry.java
@@ -8,20 +8,27 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import com.google.common.collect.ImmutableSet;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class PermissionEntry {
 	private IWorldguardPermission permission;
 	private List<Rule> rules;
+	private String checks;
 	
-	public PermissionEntry(IWorldguardPermission permission,List<Rule> rules) {
+	public PermissionEntry(IWorldguardPermission permission,String checks,List<Rule> rules) {
 		this.permission = permission;
 		this.rules = new ArrayList<>(rules);
+		this.checks = checks;
 	}
 	
 	public PermissionEntry(DataInputStream strm) throws IOException {
-		ResourceLocation loc = ResourceLocation.makeResourceLocation(strm.readUTF());
+		String[] str = strm.readUTF().split(";");
+		checks = str[1];
+		ResourceLocation loc = ResourceLocation.makeResourceLocation(str[0]);
 		permission = WorldguardRegistries.Permissions.getValue(loc);
 		int numRules = strm.readUnsignedShort();
 		rules = new ArrayList<>();
@@ -31,6 +38,15 @@ public class PermissionEntry {
 	
 	public List<Rule> getRules(){
 		return Collections.unmodifiableList(rules);
+	}
+	
+	public <T extends IForgeRegistryEntry<T>> boolean isAffectedBy(T t){
+		if(checks.equals("*"))
+			return true;
+		else if(checks.isEmpty())
+			return false;
+		else
+			return ImmutableSet.copyOf(checks.split(",\\s+")).contains(t.getRegistryName().toString());
 	}
 	
 	public IWorldguardPermission getPermission() {
